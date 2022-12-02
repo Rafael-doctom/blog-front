fb026ba3f378c775df9279e6801272ef636e3c5f
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-12-02 10:56:00 -0300

    template basic

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Front-blog</title>
+        <meta name="description" content="Front-blog" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+    </>
+  )
+}
