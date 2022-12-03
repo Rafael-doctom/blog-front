6676991e1bdd545e35a6ec92ce5f3665f7f078e8
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-12-03 15:17:39 -0300

    feat: basic details to style add

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,7 +1,15 @@
+import style from '../../../styles/login.module.css'
+
 const Login = () => {
-    return(
+    return (
         <>
-            Login
+            <div className={style.login}>
+                <div className={style.content}>
+                    <input className={style.inputs} type="text" placeholder="Insira seu nome de usuário..." />
+                    <input className={style.inputs} type="password" placeholder="Insira sua senha..." />
+                    <input className={style.inputs} type="submit" value="Fazer Login" />
+                </div>
+            </div>
         </>
     )
 }
